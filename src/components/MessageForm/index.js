2bd72b4d665244575f6e39a5a10d61d9e55ae166
CarlosEscat <carlosescat@gmail.com>
2019-08-09 17:01:05 +0200

    Ready to deploy

--- src/components/MessageForm/index.js
+++ src/components/MessageForm/index.js
@@ -9,7 +9,7 @@ export default class MessageForm extends React.Component {
     event.preventDefault();
 
     await request
-      .post("http://localhost:5000/message")
+      .post("https://mysterious-mesa-84416.herokuapp.com/message")
       .send({ message: this.state.message, user: this.props.user });
 
     this.setState({ message: "" });
