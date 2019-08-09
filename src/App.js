6753755ce931cf6def790ccd666394da9098ce6c
CarlosEscat <carlosescat@gmail.com>
2019-08-09 12:29:38 +0200

    Messages displayed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+//import { request } from "http";
+import * as request from "superagent";
 
 class App extends React.Component {
   state = {
+    message: "",
     messages: []
   };
 
@@ -9,16 +12,52 @@ class App extends React.Component {
 
   componentDidMount() {
     this.source.onmessage = event => {
-      const newMessage = [...this.state.messages, event.data];
-      this.setState({ messages: newMessage });
+      //const newMessage = [...this.state.messages, event.data];
+      const messages = JSON.parse(event.data);
+      this.setState({ messages: messages });
     };
   }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    console.log("this.state.message", this.state.message);
+
+    const response = await request
+      .post("http://localhost:5000/message")
+      .send({ message: this.state.message });
+
+    this.setState({ message: "" });
+
+    console.log("response", response);
+  };
+
+  onChange = event => {
+    const { value } = event.target;
+    this.setState({ message: value });
+  };
 
   render() {
     const messages = this.state.messages.map((messages, index) => (
       <p key={index}> {messages} </p>
     ));
-    return <main> {messages} </main>;
+
+    const form = (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={this.onChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+    );
+    return (
+      <main>
+        {" "}
+        {form}
+        {messages}{" "}
+      </main>
+    );
   }
 }
 
